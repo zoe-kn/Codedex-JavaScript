2626f1ce7fcd65888a08f0458d8cdc04d6909050
zoe <zoe2002klein@gmail.com>
2025-06-27 13:17:02 +0200

    Create 19_lucky_number.js

---
+++ 4_loops/19_lucky_number.js
@@ -0,0 +1,9 @@
+const luckyNumber = 7;
+let guess = Math.floor(Math.random()* 10) +1;
+
+while (guess !== luckyNumber) {
+  console.log("Nope it isn't", guess);
+  guess = Math.floor(Math.random()* 10) +1;
+}
+
+console.log("My lucky number is ", luckyNumber, "!");
