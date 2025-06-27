ba0acf1eb74d4468129acefffcd7eaad39cde7e0
zoe <zoe2002klein@gmail.com>
2025-06-27 11:21:39 +0200

    07_Data_Types.js

---
+++ 2_variables/07_Data_Types.js
@@ -0,0 +1,11 @@
+//Codédex
+
+const companyName = "Bosch";
+const foundingYear = 1886;
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
