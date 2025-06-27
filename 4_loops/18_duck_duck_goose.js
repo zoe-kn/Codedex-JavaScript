d97f91fdc38972daec1c9314453ea6d08a94ef2f
zoe <zoe2002klein@gmail.com>
2025-06-27 12:05:37 +0200

    18_duck_duck_goose.js

---
+++ 4_loops/18_duck_duck_goose.js
@@ -0,0 +1,10 @@
+//codédex
+
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber !=7) {
+  console.log("Duck🦆");
+  randomNumber = Math.floor(Math.random() * 7);
+}
+
+console.log("Goose🪿");
