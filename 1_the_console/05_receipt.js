065f22ce31a61333425bb056edc63d1727e5f479
zoe <zoe2002klein@gmail.com>
2025-06-27 11:18:11 +0200

    05_receipt.js

---
+++ 1_the_console/05_receipt.js
@@ -0,0 +1,14 @@
+//Codédex
+
+console.log("====================================");
+console.log("        Burger King RECEIPT         ");
+console.log("------------------------------------");
+console.log("           Order Details            ");
+console.log("Item          Quantity      Price   ");
+console.log("------------------------------------");
+console.log("Burger            1          5.99   ");
+console.log("Fries             1          2.99   ");
+console.log("Soda              1          1.99   ");
+console.log("------------------------------------");
+console.log("Total                       10.97   ");
+console.log("====================================");
