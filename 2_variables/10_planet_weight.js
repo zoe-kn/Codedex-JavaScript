032e3955b2c922757c867bb4568eb55311fa491a
zoe <zoe2002klein@gmail.com>
2025-06-27 11:24:34 +0200

    10_planet_weight.js

---
+++ 2_variables/10_planet_weight.js
@@ -0,0 +1,7 @@
+//codédex
+
+const earth_weight = 68;
+const destination_weight = earth_weight * 0.38;
+
+console.log("Your weight on Earth is "+ earth_weight + " kilogramms")
+console.log("Your weight on Mars is " + destination_weight + " kilogramms")
