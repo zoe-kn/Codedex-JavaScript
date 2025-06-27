f677750c44aacc2fcd259b312c2549ec01a76def
zoe <zoe2002klein@gmail.com>
2025-06-27 11:17:23 +0200

    04_secret_recipe.js

---
+++ 1_the_console/04_secret_recipe.js
@@ -0,0 +1,22 @@
+// Codédex
+// My secret Recipe🍪
+
+console.log("Ingredients");
+console.log("3/4 pounted salted butter (340g), softned");
+console.log("1 3/4 (350g) cups granulated sugar");
+console.log("2 egg yolks");
+console.log("3 1/2 cups (450g) flour");
+console.log("red, green or multi-colored sugar");
+
+console.log("Instructions:");
+console.log("Preheat oven to 180C.");
+console.log("In a mixer fitted with a paddle attachment, cream the butter and sugar.");
+console.log("Add the yolks and vanilla, mixing well.");
+console.log("Add the flour and combine thoroughly.")
+console.log("Use mounted teaspoonfuls and make balls of dough with your hands.");
+console.log("Place on an ungreased cookie sheet, then flatten the dough with the bottom of a pattern glass dipped in colored sugar (dont mix the colors!!!)");
+console.log("Bake for about 10 minutes (watch carefully as they burn easily), until the cookies are lightly golden just around the edges.");
+console.log("Let the cookies rest on the baking sheets for a minute or two and then gently transfer to baking racks to cool - they're fragile.");
+
+/* SECRET INGREDIENT: dont tell anyone!🤐
+A tiny dash of LOVE */
