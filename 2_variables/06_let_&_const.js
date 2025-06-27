4cc463ccaf89aff91bf3115c985de3d2960d12ff
zoe <zoe2002klein@gmail.com>
2025-06-27 11:19:44 +0200

    06_let_&_const.js

---
+++ 2_variables/06_let_&_const.js
@@ -0,0 +1,12 @@
+//Codédex
+
+
+let userName = "Zoe";
+console.log(userName);
+let favoriteColor = "babyblue";
+console.log(favoriteColor);
+
+const location = "Germany";
+console.log(location);
+const mood = "tired";
+console.log(mood);
