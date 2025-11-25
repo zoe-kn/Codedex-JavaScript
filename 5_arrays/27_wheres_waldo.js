3764b8045856625009bf975b6b03c9c39a7570ea
zoe <zoe2002klein@gmail.com>
2025-11-25 20:53:12 +0100

    Create 27_wheres_waldo.js

---
+++ 5_arrays/27_wheres_waldo.js
@@ -0,0 +1,17 @@
+
+const characters = [
+  "The Wally Watchers",
+  "Wilma",
+  "Fritz",
+  "Wizard Whitebeard",
+  "Odlaw",
+  "Waldo",
+  "Woof"
+];
+
+if (characters.includes("Waldo")) {
+  const waldoIndex = characters.indexOf("Waldo");
+  console.log("Found Waldo at index " + waldoIndex);
+} else {
+  console.log("Not Found");
+}
