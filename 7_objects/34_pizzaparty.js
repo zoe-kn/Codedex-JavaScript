3e6ef0e2127b8a4bca98b2e27bb6a3a903f2ed34
zoe <zoe2002klein@gmail.com>
2025-11-27 09:06:09 +0100

    Create 34_pizzaparty.js

---
+++ 7_objects/34_pizzaparty.js
@@ -0,0 +1,18 @@
+
+// Pizza 1
+
+const pizzaTopping = "Cheese 🧀";
+const pizzaType = "Pan";
+const pizzaSlices = 8;
+const pizzaPrice = 12.99;
+
+// Pizza 2 
+
+const pizza = {
+  topping: "Pepperoni 🍕",
+  type: "Hand-tossed",
+  slices: 12,
+  price: 14.99
+}
+
+console.log(pizza);
