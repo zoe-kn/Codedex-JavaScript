d8e00e3f7f205540ef9fe2e21ce9d6472bb9e965
zoe <zoe2002klein@gmail.com>
2025-12-03 22:41:47 +0100

    Create script.js

---
+++ 8_trifecta/41_hotline_bling/script.js
@@ -0,0 +1,7 @@
+let drakePicTwo = document.getElementById("drake-pic-2");
+
+let heading = document.getElementById("heading-2");
+
+drakePicTwo.src = "https://i.imgur.com/RGbh6zY.png";
+
+heading.innerText = "Learn by Doing";
