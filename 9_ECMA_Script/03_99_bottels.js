8308e6f4cd560238969063192545df674dd99ae9
zoe <zoe2002klein@gmail.com>
2025-12-08 15:11:40 +0100

    Create 03_99_bottels.js

---
+++ 9_ECMA_Script/03_99_bottels.js
@@ -0,0 +1,9 @@
+
+
+for (let i = 99; i > 0; i--) {
+  console.log(`${i} bottles of pop on the wall"`);
+  console.log(`${i} bottles of pop"`);
+  console.log("take one down, pass it around");
+  console.log(`${i-1} bottles of pop on the wall`);
+  console.log();
+}
